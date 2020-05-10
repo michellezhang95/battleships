7e186dfd9a79c1ad5a140080da5b78e39a8e294b
Michelle <michelle.zhang56@gmail.com>
2020-05-10 22:28:00 +1200

    board initialise function done - still wip

--- Battleships - Michelle Zhang/main.cpp
+++ Battleships - Michelle Zhang/main.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>;
 #include <windows.h>;
+#include <string>;
 
 using namespace std;
 
 // player and AI player
-// same functions but AI player doesn't require user input, just automatically calculate positions
+// same functions but AI player doesn"t require user input, just automatically calculate positions
 
 //classes
 // player
@@ -12,32 +13,33 @@ using namespace std;
 
 // variables
 // set battleships as arrays with their respective sizes
-char air_carrier[5] = {'A', 'A', 'A', 'A', 'A'};
-char battleship[4] = { 'B','B','B','B' };
-char destroyer[3] = { 'D','D','D' };
-char submarine[3] = { 'S','S','S' };
-char patrol_boat[2] = { 'P','P' };
+string air_carrier[5] = {"A", "A", "A", "A", "A"};
+string battleship[4] = { "B","B","B","B" };
+string destroyer[3] = { "D","D","D" };
+string submarine[3] = { "S","S","S" };
+string patrol_boat[2] = { "P","P" };
 
 //functions
 // before game begins players choose where to put their ships (2) main should call this function
-//			to begin with, only draw player's board so that they can place their ship
+//			to begin with, only draw player"s board so that they can place their ship
 //			player can choose if the ship is placed horizontally or vertically
 //			within this function, call start function when ready
 // start/play function (1 nano sub)
-//			in this function, then draw enemy board next to the player's board
+//			in this function, then draw enemy board next to the player"s board
 // RNG choose who starts (1 nano sub)
 // toggle turn passing (1) 
 // re-draw function after each turn ,gotoxy (1) 
 // end game function, restart (1)
-void placeShips();
+void initialise();
+void drawBoard();
 void GotoXY(int, int);
 
 //data structures
 // 2d array to store game board changes
-char gameBoard[12][12];
+string gameBoard[12][12];
 
 int main() {
-	placeShips();
+	initialise();
 
 	int iTemp;
 	cin >> iTemp;
@@ -49,78 +51,113 @@ void GotoXY(int x, int y) {
 	point.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
 }
-void placeShips() {
+void initialise() {
 	cout << endl; 
 	//
 	cout << "		Player Board" << endl << endl;
 	//for each row 
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < 11; i++) {
 		//for each column
-		cout << " ";
-		for (int j = 0; j < 12; j++) {
+		
+		for (int j = 0; j < 11; j++) {
 			if (i == 0) {
 				switch (j) {
+				case 1:
+					gameBoard[i][j] = "1";
+					break;
 				case 2:
-					gameBoard[i][j] = '1';
+					gameBoard[i][j] = "2";
+					break;
+				case 3:
+					gameBoard[i][j] = "3";
+					break;
+				case 4:
+					gameBoard[i][j] = "4";
+					break;
+				case 5:
+					gameBoard[i][j] = "5";
+					break;
+				case 6:
+					gameBoard[i][j] = "6";
+					break;
+				case 7:
+					gameBoard[i][j] = "7";
+					break;
+				case 8:
+					gameBoard[i][j] = "8";
+					break;
+				case 9:
+					gameBoard[i][j] = "9";
+					break;
+				case 10:
+					gameBoard[i][j] = "10";
+					break;
+				default:
 					break;
 				}
-				cout << gameBoard[i][j];
-				
-
 			}
-			else {
-
-			}
-			if (j == 10) {
-				cout << endl << endl;
-			} 
+			
 			else {
 				if (j == 0) {
 					switch (i) {
 					case 1:
-						gameBoard[i][j] = 'A';
+						gameBoard[i][j] = "A";
 						break;
 					case 2:
-						gameBoard[i][j] = 'B';
+						gameBoard[i][j] = "B";
 						break;
 					case 3: 
-						gameBoard[i][j] = 'C';
+						gameBoard[i][j] = "C";
 						break;
 					case 4: 
-						gameBoard[i][j] = 'D';
+						gameBoard[i][j] = "D";
 						break;
 					case 5: 
-						gameBoard[i][j] = 'E';
+						gameBoard[i][j] = "E";
 						break;
 					case 6:
-						gameBoard[i][j] = 'F';
+						gameBoard[i][j] = "F";
 						break;
 					case 7: 
-						gameBoard[i][j] = 'G';
+						gameBoard[i][j] = "G";
 						break;
 					case 8:
-						gameBoard[i][j] = 'H';
+						gameBoard[i][j] = "H";
 						break;
 					case 9:
-						gameBoard[i][j] = 'I';
+						gameBoard[i][j] = "I";
 						break;
 					case 10:
-						gameBoard[i][j] = 'J';
+						gameBoard[i][j] = "J";
 						break;
 					default:
 						break;
 					}
 				}
 				else {
-					gameBoard[i][j] = 'x';
+					gameBoard[i][j] = "x";
 				}
-				cout << gameBoard[i][j];
-				cout << "    ";	
 			}
+			
 		}
 	}
 	//
+	drawBoard();
 	cout << "To start, place your ships on the board...";
-	GotoXY(60, 5);
+	//GotoXY(60, 5);
+	
 
 }
+
+	void drawBoard() {
+		for (int i = 0; i < 11; i++) {
+			if (i == 0) cout << "     ";
+			for (int j = 0; j < 12; j++) {
+				if (j == 11) {
+					cout << endl << endl;
+				}
+				cout << gameBoard[i][j];
+				cout << "    ";
+			}
+		}
+	}
